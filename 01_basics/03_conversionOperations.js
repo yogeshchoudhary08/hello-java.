5493b44dcb7f0a7fb372f1b1b01db4081f550820
jaat500 <137821894+jaat500@users.noreply.github.com>
2024-06-11 10:03:59 +0000

    comparison in dayatype

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -17,5 +17,33 @@ let bolleanIsLoggedIn = Boolean(isLoggedIn);
  //1 => true , 0=> false
  //"" => false 
  //"yogesh choudahry" => true
- let sumNumber = String(someNumber);
-console.log(typeof sumNumber);
+ //let sumNumber = String(someNumber);
+//console.log(typeof sumNumber);
+
+
+
+
+
+let value = 3 
+let negValue = -value
+//console.log(negValue);
+ 
+
+//console.log(2+2.);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);//power 
+//console.log(2/3);
+//console.log(2%3);
+
+//console.log(true);//true
+//console.log(+"");//zero, but not perfer to use
+
+
+let num1 ,num2 ,num3 
+num1=num2=num3 = 2+2 ;//but not perfer to use.
+
+
+let gamneCounter = 100
+++gamneCounter;//gameCounter++;//prefix operator , post fix operator...
+console.log(gamneCounter);
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+
+
+//console.log("2">1);
+//console.log("02">1);
+
+
+//console.log(null > 0);
+//console.log(null == 0);//
+//console.log(null>=0);// null was converted 0.
+
+//console.log(undefined==0);
+
+//=== check data type
+
+
+//console.log("2"===2);
