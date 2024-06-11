081f67bfb8d59d4da9a1f84ab2bc3499f78c7c6c
jaat500 <137821894+jaat500@users.noreply.github.com>
2024-06-11 07:47:01 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,26 @@
+//"use strict"; // treat all js code as new version// 
+
+//alert("hello");//
+//alert(9+9); we are using node.js, not browser.//
+
+
+//console.log(3 +3)//
+
+//console.log("yogesh choudhary") code readablillity should be high.. both will excuted but both are not good path to write code..//
+let name = "yogesh choudhary" // string//
+let age = 2 ; // number => 2 to power 53//
+//bigint//
+//boolean => true,false //
+// string => ""
+// null => standalone value secial type..
+//undefined =>
+    // symbol=> figma tools.. unique..
+
+
+//object => 
+
+console.log(typeof"yogesh choudhary");
+console.log(typeof null);// objrct//
+
+
+
