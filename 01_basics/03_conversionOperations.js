6bde383980e645f1ba096677f8585c1f3874852c
jaat500 <137821894+jaat500@users.noreply.github.com>
2024-06-11 09:14:23 +0000

    conversion of data types.

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+let score = undefined
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number (score);
+//console.log( typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"43" => easliy convert 43
+//"43hdhd"=>NaN
+//true=> 1; false =>0;
+
+let isLoggedIn = "";
+let bolleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(bolleanIsLoggedIn);
+ //1 => true , 0=> false
+ //"" => false 
+ //"yogesh choudahry" => true
+ let sumNumber = String(someNumber);
+console.log(typeof sumNumber);
