ae244ee33f5b77d78b5cb67ffd6d328a72cc15f2
jaat500 <137821894+jaat500@users.noreply.github.com>
2024-06-12 06:59:25 +0000

    datatypes-summary

---
+++ datatypes-summary.js
@@ -0,0 +1,39 @@
+// primitive
+
+// 7 types: string,number,bollean,null,undefined,symbol,BigInt..
+
+const score = 100 
+const scoreValue = 100 
+
+const isLoggedIn= false
+
+const outSideTemp =null
+
+let userEmail;
+
+const id = symbol('123')
+const anotherId = symbol('123')
+//console.log(id === anotherId);
+
+
+const bigNumber = 12255564446n
+
+
+
+
+//reference typr (Non primitive)
+
+//Array, Objects , Functions
+
+//const heros = ["shaktiman ", "chota bheem","chutki"]
+
+
+let myself ={ 
+    name : "yogesh",
+    age: 21,
+}
+//const myFunction= function(){
+    console.log("hello dev..!");
+
+
+console.log(typeof myFunction);
