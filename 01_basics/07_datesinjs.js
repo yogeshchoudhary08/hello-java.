a13484beac0dd2d22a7b826394df349c3da248a9
jaat500 <137821894+jaat500@users.noreply.github.com>
2024-06-12 09:50:42 +0000

    date methodes in js

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,12 @@
+/// dates
+
+
+let myDate = new Date()
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
