ef8a8988dd81a1125e88ebb0f4e26cb5d0688c26
jaat500 <137821894+jaat500@users.noreply.github.com>
2024-06-15 12:25:25 +0000

    IIFE  function in js

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,14 @@
+// immediately invoked function expressions (IIFE)
+
+(function chai (){  // remove for globsl scopes polluction live ver lots things
+    //console.log(` DB CONNECTED`);
+})();// use semi colon for end the statement..! for excute net code
+// chai()
+
+(   (name) => { // namned iife
+    console.log(`DB CONNECTED TWO ${name}`);
+})(`yogesh`)
+ 
+
+// for excute two iife remove semi colon from first code..!
+
